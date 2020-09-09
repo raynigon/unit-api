643fdd8c6357e8fa782a77ed24b6112877aa1b5e
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2020-09-09 10:15:18 +0200

    Fix Springdoc

--- springdoc-starter/src/main/java/com/raynigon/unit_api/springdoc/UnitApiPropertyCustomizer.java
+++ springdoc-starter/src/main/java/com/raynigon/unit_api/springdoc/UnitApiPropertyCustomizer.java
@@ -6,6 +6,7 @@ import com.raynigon.unit_api.jackson.annotation.JsonUnit;
 import io.swagger.v3.core.converter.AnnotatedType;
 import io.swagger.v3.oas.models.media.Schema;
 import org.springdoc.core.customizers.PropertyCustomizer;
+import tech.units.indriya.unit.Units;
 
 import javax.measure.Quantity;
 import javax.measure.Unit;
@@ -18,9 +19,9 @@ public class UnitApiPropertyCustomizer implements PropertyCustomizer {
     public Schema<?> customize(Schema property, AnnotatedType type) {
         if (isApplicable(type.getType())) {
             Unit<?> unit = resolveUnit(type);
-            property.setType("string");
+            property.setType("number | string"); // could be configurable
             property.setProperties(null);
-            property.setDescription(buildDescription(unit, property.getDescription()));
+            property.setDescription(buildDescription(property.getName(), unit, property.getDescription()));
             property.setExample("1" + (unit.getSymbol() != null ? " " + unit.getSymbol() : ""));
         }
         return property;
@@ -46,14 +47,44 @@ public class UnitApiPropertyCustomizer implements PropertyCustomizer {
                 Quantity.class.isAssignableFrom(((SimpleType) type).getRawClass());
     }
 
-    private String buildDescription(Unit<?> unit, String description) {
+    private String buildDescription(String name, Unit<?> unit, String description) {
         String result = "";
         if (unit != null) {
-            result += unit.getName() + (unit.getSymbol() != null ? " in " + unit.getSymbol() : "");
+            String unitName = getUnitName(unit);
+            String unitSymbol = getUnitSymbol(unit);
+
+            // could be localized
+            result += name + " is given in " + unitName;
+            if (unitSymbol != null) {
+                result += " (" + unitSymbol + ")";
+            }
         }
         if (description != null) {
-            result += result.equals("") ? description : "\n" + description;
+            if (!result.equals("")) {
+                result += "\n";
+            }
+            result += description;
         }
         return result;
+    }
+
+    /**
+     *  Temporary Solution until we figure out something better
+     */
+    private String getUnitSymbol(Unit<?> unit) {
+        if (unit.getSymbol() != null) return unit.getSymbol();
+        if (unit == Units.METRE_PER_SECOND) return "m/s";
+        if (unit == Units.KILOMETRE_PER_HOUR) return "km/h";
+        return null;
+    }
+
+    /**
+     *  Temporary Solution until we figure out something better
+     */
+    private String getUnitName(Unit<?> unit) {
+        if (unit.getName() != null) return unit.getName();
+        if (unit == Units.METRE_PER_SECOND) return "metre per second";
+        if (unit == Units.KILOMETRE_PER_HOUR) return "kilometre per hour";
+        return "unknown";
     }
 }
