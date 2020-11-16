0018e91a67476de78c166acc7d4c38c7d8f1d9ae
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2020-11-16 19:08:03 +0100

    Fix #16 Remove unused Imports

--- jackson-module/src/main/java/com/raynigon/unit_api/jackson/exception/UnknownUnitException.java
+++ jackson-module/src/main/java/com/raynigon/unit_api/jackson/exception/UnknownUnitException.java
@@ -1,8 +1,6 @@
 package com.raynigon.unit_api.jackson.exception;
 
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.raynigon.unit_api.core.units.general.IUnit;
 
 import javax.measure.Quantity;
 import java.io.Closeable;
