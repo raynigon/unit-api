ffd47803d31c7dbf9ef85b6bf20187394c06d3c6
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2021-03-24 14:14:05 +0100

    Add Volume and Area Units

--- unit-api-core/src/main/java/com/raynigon/unit_api/core/function/unitconverter/AddConverter.java
+++ unit-api-core/src/main/java/com/raynigon/unit_api/core/function/unitconverter/AddConverter.java
@@ -48,102 +48,110 @@ import javax.measure.UnitConverter;
  */
 public final class AddConverter extends AbstractConverter implements ValueSupplier<Number> {
 
-  /** */
-  private static final long serialVersionUID = -2981335308595652284L;
-  /** Holds the offset. */
-  private final Number offset;
+    /**
+     *
+     */
+    private static final long serialVersionUID = -2981335308595652284L;
+    /**
+     * Holds the offset.
+     */
+    private final Number offset;
 
-  /**
-   * Creates an additive converter having the specified offset.
-   *
-   * @param offset the offset value.
-   */
-  public AddConverter(Number offset) {
-    this.offset = CalculusUtils.currentNumberSystem().narrow(offset);
-  }
-
-  /**
-   * Returns the offset value for this add converter.
-   *
-   * @return the offset value.
-   */
-  public Number getOffset() {
-    return offset;
-  }
-
-  @Override
-  public boolean isIdentity() {
-    return CalculusUtils.currentNumberSystem().isZero(offset);
-  }
-
-  @Override
-  protected boolean canReduceWith(AbstractConverter that) {
-    return that instanceof AddConverter;
-  }
-
-  @Override
-  protected AbstractConverter reduce(AbstractConverter that) {
-    NumberSystem ns = CalculusUtils.currentNumberSystem();
-    Number newOffset = ns.add(offset, ((AddConverter) that).offset);
-    return new AddConverter(newOffset);
-  }
-
-  @Override
-  public AddConverter inverseWhenNotIdentity() {
-    NumberSystem ns = CalculusUtils.currentNumberSystem();
-    Number newOffset = ns.negate(offset);
-    return new AddConverter(newOffset);
-  }
-
-  @Override
-  protected Number convertWhenNotIdentity(Number value) {
-    return Calculator.of(offset).add(value).peek();
-  }
-
-  @Override
-  public String transformationLiteral() {
-    NumberSystem ns = CalculusUtils.currentNumberSystem();
-    int signum = ns.signum(offset);
-    return String.format("x -> x %s %s", signum < 0 ? "-" : "+", ns.abs(offset));
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (obj instanceof AddConverter) {
-      AddConverter other = (AddConverter) obj;
-      return Objects.equals(offset, other.offset);
+    /**
+     * Creates an additive converter having the specified offset.
+     *
+     * @param offset the offset value.
+     */
+    public AddConverter(Number offset) {
+        this.offset = CalculusUtils.currentNumberSystem().narrow(offset);
     }
 
-    return false;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hashCode(offset);
-  }
-
-  @Override
-  public boolean isLinear() {
-    return isIdentity();
-  }
-
-  @Override
-  public Number getValue() {
-    return offset;
-  }
-
-  @Override
-  public int compareTo(UnitConverter o) {
-    if (this == o) {
-      return 0;
+    public static UnitConverter of(double offset) {
+        return new AddConverter(offset);
     }
-    if (o instanceof AddConverter) {
-      NumberSystem ns = CalculusUtils.currentNumberSystem();
-      return ns.compare(this.getValue(), ((AddConverter) o).getValue());
+
+    /**
+     * Returns the offset value for this add converter.
+     *
+     * @return the offset value.
+     */
+    public Number getOffset() {
+        return offset;
     }
-    return -1;
-  }
+
+    @Override
+    public boolean isIdentity() {
+        return CalculusUtils.currentNumberSystem().isZero(offset);
+    }
+
+    @Override
+    protected boolean canReduceWith(AbstractConverter that) {
+        return that instanceof AddConverter;
+    }
+
+    @Override
+    protected AbstractConverter reduce(AbstractConverter that) {
+        NumberSystem ns = CalculusUtils.currentNumberSystem();
+        Number newOffset = ns.add(offset, ((AddConverter) that).offset);
+        return new AddConverter(newOffset);
+    }
+
+    @Override
+    public AddConverter inverseWhenNotIdentity() {
+        NumberSystem ns = CalculusUtils.currentNumberSystem();
+        Number newOffset = ns.negate(offset);
+        return new AddConverter(newOffset);
+    }
+
+    @Override
+    protected Number convertWhenNotIdentity(Number value) {
+        return Calculator.of(offset).add(value).peek();
+    }
+
+    @Override
+    public String transformationLiteral() {
+        NumberSystem ns = CalculusUtils.currentNumberSystem();
+        int signum = ns.signum(offset);
+        return String.format("x -> x %s %s", signum < 0 ? "-" : "+", ns.abs(offset));
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj instanceof AddConverter) {
+            AddConverter other = (AddConverter) obj;
+            return Objects.equals(offset, other.offset);
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(offset);
+    }
+
+    @Override
+    public boolean isLinear() {
+        return isIdentity();
+    }
+
+    @Override
+    public Number getValue() {
+        return offset;
+    }
+
+    @Override
+    public int compareTo(UnitConverter o) {
+        if (this == o) {
+            return 0;
+        }
+        if (o instanceof AddConverter) {
+            NumberSystem ns = CalculusUtils.currentNumberSystem();
+            return ns.compare(this.getValue(), ((AddConverter) o).getValue());
+        }
+        return -1;
+    }
 }
---
+++ unit-api-core/src/main/java/com/raynigon/unit_api/core/units/si/volume/CubicMetre.java
@@ -0,0 +1,17 @@
+package com.raynigon.unit_api.core.units.si.volume;
+
+import com.raynigon.unit_api.core.units.general.AlternateUnit;
+import com.raynigon.unit_api.core.units.si.SISystem;
+import com.raynigon.unit_api.core.units.si.area.SquareMetre;
+import com.raynigon.unit_api.core.units.si.length.Metre;
+
+import javax.measure.Unit;
+import javax.measure.quantity.Volume;
+
+public class CubicMetre extends AlternateUnit<Volume> {
+
+    @SuppressWarnings("unchecked")
+    public CubicMetre() {
+        super(SISystem.ID, "m³", "Cubic Metre", (Unit<Volume>) new Metre().pow(3), Volume.class);
+    }
+}
