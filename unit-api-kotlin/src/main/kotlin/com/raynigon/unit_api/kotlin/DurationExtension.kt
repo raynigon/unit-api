6fce015d5d0de602f93befdb0b3e2cdcc0ebf6bc
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2021-08-13 18:46:15 +0200

    Add Code Documentation

--- unit-api-kotlin/src/main/kotlin/com/raynigon/unit_api/kotlin/DurationExtension.kt
+++ unit-api-kotlin/src/main/kotlin/com/raynigon/unit_api/kotlin/DurationExtension.kt
@@ -9,12 +9,20 @@ import javax.measure.quantity.Time
 import com.raynigon.unit_api.core.units.si.SISystemUnitsConstants.NanoSecond as cNanoSecond
 import com.raynigon.unit_api.core.units.si.SISystemUnitsConstants.Second as cSecond
 
+/**
+ * Convert a Java Duration to a Time Quantity.
+ * @return the Quantity<Time> object with nano second resolution
+ */
 fun Duration.toQuantity(): Quantity<Time> {
     if (this.nano == 0)
         return cSecond(this.seconds)
     return (cSecond(this.seconds) + cNanoSecond(this.nano))
 }
 
+/**
+ * Convert a Time Quantity to a Java Duration.
+ * @return the Java Duration object with nano second resolution
+ */
 fun Quantity<Time>.toDuration(): Duration {
     val seconds: Long = this.to(Second()).value.toLong()
     val nanos: Long = (this - cSecond(seconds)).to(NanoSecond()).value.toLong()
