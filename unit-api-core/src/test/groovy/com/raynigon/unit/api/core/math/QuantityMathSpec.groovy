60e05fb527e9cc2824f8a4875ee1316c693e17be
simon <10846939+raynigon@users.noreply.github.com>
2024-01-11 11:22:43 +0100

    Add min/max functions for Quantities

---
+++ unit-api-core/src/test/groovy/com/raynigon/unit/api/core/math/QuantityMathSpec.groovy
@@ -0,0 +1,80 @@
+package com.raynigon.unit.api.core.math
+
+import spock.lang.Specification
+
+import static com.raynigon.unit.api.core.units.si.SISystemUnitsConstants.Metre
+
+class QuantityMathSpec extends Specification {
+
+    def "minimum - #a, #b"() {
+        expect:
+        QuantityMath.min(a, b) == c
+        where:
+        a        | b         | c
+        Metre(0) | Metre(1)  | Metre(0)
+        Metre(2) | Metre(1)  | Metre(1)
+        Metre(0) | Metre(0)  | Metre(0)
+        Metre(0) | Metre(-1) | Metre(-1)
+    }
+
+    def "maximum - #a, #b"() {
+        expect:
+        QuantityMath.max(a, b) == c
+        where:
+        a         | b         | c
+        Metre(0)  | Metre(1)  | Metre(1)
+        Metre(2)  | Metre(1)  | Metre(2)
+        Metre(0)  | Metre(0)  | Metre(0)
+        Metre(-2) | Metre(-1) | Metre(-1)
+    }
+
+    def "minimum - #a, #b, #c"() {
+        expect:
+        QuantityMath.min(a, b, c) == z
+        where:
+        a         | b         | c         | z
+        Metre(0)  | Metre(1)  | Metre(2)  | Metre(0)
+        Metre(2)  | Metre(1)  | Metre(1)  | Metre(1)
+        Metre(0)  | Metre(0)  | Metre(0)  | Metre(0)
+        Metre(-3) | Metre(-2) | Metre(-1) | Metre(-3)
+    }
+
+    def "maximum - #a, #b, #c"() {
+        expect:
+        QuantityMath.max(a, b, c) == z
+        where:
+        a         | b         | c         | z
+        Metre(0)  | Metre(1)  | Metre(2)  | Metre(2)
+        Metre(3)  | Metre(2)  | Metre(1)  | Metre(3)
+        Metre(0)  | Metre(0)  | Metre(0)  | Metre(0)
+        Metre(-3) | Metre(-2) | Metre(-1) | Metre(-1)
+    }
+
+    def "minimum with null"() {
+        when:
+        QuantityMath.min(a, b)
+
+        then:
+        thrown(NullPointerException)
+
+        where:
+        a        | b
+        null     | null
+        Metre(0) | null
+        null     | Metre(0)
+    }
+
+    def "maximum with null"() {
+        when:
+        QuantityMath.min(a, b)
+
+        then:
+        thrown(NullPointerException)
+
+        where:
+        a        | b
+        null     | null
+        Metre(0) | null
+        null     | Metre(0)
+    }
+}
