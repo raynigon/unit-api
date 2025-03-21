32aeb471791dc2d2c4aa1f79041053f00c00e170
Carlo Goetz <carlo.goetz@inovex.de>
2025-03-21 11:00:37 +0100

    fix parsing of values with scientific notation

--- unit-api-core/src/test/groovy/com/raynigon/unit/api/core/io/DefaultQuantityReaderSpec.groovy
+++ unit-api-core/src/test/groovy/com/raynigon/unit/api/core/io/DefaultQuantityReaderSpec.groovy
@@ -5,11 +5,14 @@ import spock.lang.Unroll
 
 import static com.raynigon.unit.api.core.units.si.SISystemUnitsConstants.Celsius
 import static com.raynigon.unit.api.core.units.si.SISystemUnitsConstants.Metre
+import static com.raynigon.unit.api.core.units.si.SISystemUnitsConstants.Number
+import static com.raynigon.unit.api.core.units.si.SISystemUnitsConstants.CubicMetre
+import static com.raynigon.unit.api.core.units.si.SISystemUnitsConstants.Kilometre
 
 class DefaultQuantityReaderSpec extends Specification {
 
     @Unroll
-    def '#input is read as #output'() {
+    def '"#input" is read as #output'() {
         given:
         def reader = new DefaultQuantityReader()
 
@@ -21,9 +24,19 @@ class DefaultQuantityReaderSpec extends Specification {
 
         where:
         input    | output
+        "1" | Number(1)
+        "123" | Number(123)
         "1 m"    | Metre(1)
         "10 m"   | Metre(10)
         "10.2 m" | Metre(10.2)
         "1 ℃"    | Celsius(1)
+        "1℃"    | Celsius(1)
+        "0.1e-10 m" | Metre(0.1e-10)
+        "1234.5E10 m³" | CubicMetre(1.2345e13)
+        ".123 m" |  Metre(0.123)
+        "123m" | Metre(123)
+        "12. km" | Kilometre(12)
+        " " | null
+        "" | null
     }
 }
