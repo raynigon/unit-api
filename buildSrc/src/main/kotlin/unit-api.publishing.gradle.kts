93e71f801322a35ad8c52c8be7d82c442cc553ca
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2026-01-09 15:07:42 +0100

    Update Maven repository URL for OSSRH

--- buildSrc/src/main/kotlin/unit-api.publishing.gradle.kts
+++ buildSrc/src/main/kotlin/unit-api.publishing.gradle.kts
@@ -23,7 +23,7 @@ publishing {
     repositories {
         maven {
             name = "OSSRH" // OSS Repository Hosting
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            url = uri("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/")
             credentials {
                 username = System.getenv("OSSRH_USERNAME") ?: null
                 password = System.getenv("OSSRH_PASSWORD") ?: null
